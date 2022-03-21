9a3ddbd9fc030401292531a6be8db43585f8fb2b
Sebastián <juansebasmorenoruiz@gmail.com>
2022-03-20 20:35:56 -0500

    updating ItemService.updateItem to meet requirements

--- src/main/java/com/perficient/praxis/gildedrose/business/ItemService.java
+++ src/main/java/com/perficient/praxis/gildedrose/business/ItemService.java
@@ -3,96 +3,99 @@ package com.perficient.praxis.gildedrose.business;
 import com.perficient.praxis.gildedrose.error.ResourceNotFoundException;
 import com.perficient.praxis.gildedrose.model.Item;
 import com.perficient.praxis.gildedrose.repository.ItemRepository;
-import org.springframework.stereotype.Service;
-
 import java.util.Arrays;
 import java.util.List;
+import org.springframework.stereotype.Service;
 
 @Service
 public class ItemService {
 
-    private final ItemRepository itemRepository;
+	private final ItemRepository itemRepository;
 
-    Item[] items;
+	Item[] items;
 
-    public ItemService(ItemRepository itemRepository, Item[] items) {
-        this.itemRepository = itemRepository;
-        this.items = items;
-    }
+	public ItemService(ItemRepository itemRepository, Item[] items) {
+		this.itemRepository = itemRepository;
+		this.items = items;
+	}
 
-    public List<Item> updateQuality() {
-        var itemsList = itemRepository.findAll();
-        var items = itemsList.toArray(new Item[itemsList.size()]);
+	public List<Item> updateQuality() {
+		var itemsList = itemRepository.findAll();
+		var items = itemsList.toArray(new Item[itemsList.size()]);
 
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].type.equals(Item.Type.AGED)
-                    && !items[i].type.equals(Item.Type.TICKETS)) {
-                if (items[i].quality > 0) {
-                    if (!items[i].type.equals(Item.Type.LEGENDARY)) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+		for (int i = 0; i < items.length; i++) {
+			if (!items[i].type.equals(Item.Type.AGED)
+				&& !items[i].type.equals(Item.Type.TICKETS)) {
+				if (items[i].quality > 0) {
+					if (!items[i].type.equals(Item.Type.LEGENDARY)) {
+						items[i].quality = items[i].quality - 1;
+					}
+				}
+			} else {
+				if (items[i].quality < 50) {
+					items[i].quality = items[i].quality + 1;
 
-                    if (items[i].type.equals(Item.Type.TICKETS)) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
+					if (items[i].type.equals(Item.Type.TICKETS)) {
+						if (items[i].sellIn < 11) {
+							if (items[i].quality < 50) {
+								items[i].quality = items[i].quality + 1;
+							}
+						}
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
+						if (items[i].sellIn < 6) {
+							if (items[i].quality < 50) {
+								items[i].quality = items[i].quality + 1;
+							}
+						}
+					}
+				}
+			}
 
-            if (!items[i].type.equals(Item.Type.LEGENDARY)) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
+			if (!items[i].type.equals(Item.Type.LEGENDARY)) {
+				items[i].sellIn = items[i].sellIn - 1;
+			}
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].type.equals(Item.Type.AGED)) {
-                    if (!items[i].type.equals(Item.Type.TICKETS)) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].type.equals(Item.Type.LEGENDARY)) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
-            }
-            itemRepository.save(items[i]);
-        }
-        return Arrays.asList(items);
-    }
+			if (items[i].sellIn < 0) {
+				if (!items[i].type.equals(Item.Type.AGED)) {
+					if (!items[i].type.equals(Item.Type.TICKETS)) {
+						if (items[i].quality > 0) {
+							if (!items[i].type.equals(Item.Type.LEGENDARY)) {
+								items[i].quality = items[i].quality - 1;
+							}
+						}
+					} else {
+						items[i].quality = items[i].quality - items[i].quality;
+					}
+				} else {
+					if (items[i].quality < 50) {
+						items[i].quality = items[i].quality + 1;
+					}
+				}
+			}
+			itemRepository.save(items[i]);
+		}
+		return Arrays.asList(items);
+	}
 
 
-    public Item createItem(Item item) {
-        return itemRepository.save(item);
-    }
+	public Item createItem(Item item) {
+		return itemRepository.save(item);
+	}
 
-    public Item updateItem(int id, Item item) {
-        return itemRepository.save(new Item(id, item.name, item.sellIn, item.quality, item.type));
-    }
+	public Item updateItem(int id, Item item) {
+		if (itemRepository.existsById(id)) {
+			return itemRepository.save(new Item(id, item.name, item.sellIn, item.quality, item.type));
+		} else {
+			throw new ResourceNotFoundException("The item you are trying to update does not exist");
+		}
+	}
 
-    public List<Item> listItems(){
-        return itemRepository.findAll();
-    }
+	public List<Item> listItems() {
+		return itemRepository.findAll();
+	}
 
-    public Item findById(int id) {
-        return itemRepository.findById(id).orElseThrow(
-                ()-> new ResourceNotFoundException(""));
-    }
+	public Item findById(int id) {
+		return itemRepository.findById(id).orElseThrow(
+			() -> new ResourceNotFoundException(""));
+	}
 }
